fc7f099b0cb12108845be67fa707410788ce691d
Andy Lin <andylin1996@gmail.com>
2016-03-29 09:54:06 -0700

    Add rfid library initialization

--- Master/Master.ino
+++ Master/Master.ino
@@ -9,15 +9,18 @@ Servo myservo;  //Create a new servo object to control the cookie arm
 myLCD lcd(0, 1, 2, 3, 4, 5);
 boolean displayTitle;
 
+RFID rfid();
+unsigned long last_card_read = 0;
 
 void setup() {
   myservo.attach(3);  // attaches the servo on pin 3 to the servo object
   lcd.initialize();
   displayTitle = true;
+  rfid.initialize();
 }
 
 void loop() {
-
+  checkID();
 }
 
 void dispenseCooke(){
@@ -36,5 +39,11 @@ void displayLCD(){
     delay(1000);
  }
   displayTitle = false;
+}
+
+void checkID(){
+  if (rfid.is_available()){
+    last_card_read = rfid.get_rfid_id();
+  }
 }
 
