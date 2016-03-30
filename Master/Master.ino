488ac1bf34bb975da322caf23a683f32d3c9e80a
Angy Chung <angyychung@gmail.com>
2016-03-29 23:49:42 -0700

    add header file for constants

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,22 +1,25 @@
+#include "Master_constants.h"
 #include <Servo.h>
 #include <myLCD.h>
+#include <Rfid.h>
 #include <stdio.h>
 #include <string.h>
-#include "Rfid.h"
 
-Servo myservo;  //Create a new servo object to control the cookie arm
+Servo cookie_servo;  //Create a new servo object to control the cookie arm
 
 // rs, en, db0, db1, db2, db3
-myLCD lcd(0, 1, 2, 3, 4, 5);
+myLCD lcd(LCD_RS, LCD_EN, LCD_DB0, LCD_DB1, LCD_DB2, LCD_DB3);
 boolean displayTitle;
 
-RFID rfid();
+RFID rfid;
 unsigned long last_card_read = 0;
 
 void setup() {
-  myservo.attach(3);  // attaches the servo on pin 3 to the servo object
+  cookie_servo.attach(COOKIE_SERVO);  // attaches the servo on pin 3 to the servo object
+  
   lcd.initialize();
   displayTitle = true;
+  
   rfid.initialize();
 }
 
@@ -36,27 +39,27 @@ void loop() {
   boolean accessGranted;
   
   if(accessGranted){
-    printLCD("YOU GET A COOKIE!!11");
+    lcd.printLCD("YOU GET A COOKIE!!11");
     dispenseCookie();       //dispense cookie
     delay(1000);
-    last_card_read = 0;     //resets last card id
   }
   else{
-    printLCD("RE-FUCKING-JECTED");
+    lcd.printLCD("RE-FUCKING-JECTED");
     delay(2000);
-    last_card_read = 0;      //resets last card id
   }
-  clear();                   //resets lcd
+  
+  last_card_read = 0;      //resets last card id
+  lcd.clear();                   //resets lcd
   lcd.cursorTo(0, 0);
 }
 
 // This function dispenses one cookie when called
 void dispenseCookie(){
-  myservo.write(0);
+  cookie_servo.write(0);
   delay(2000);
-  myservo.write(60);
+  cookie_servo.write(60);
   delay(2000);
-  myservo.write(0);
+  cookie_servo.write(0);
 }
 
 //Welcome message
@@ -67,7 +70,7 @@ void displayLCDWelcome(){
     delay(1000);
 }
 
-//This function uses an RFID and checks for card and reads its' ID 
+//This function uses an RFID and checks for card and reads its ID 
 //returns ID
 void checkID(){
   if (rfid.is_available()){
---
+++ Master/Master_constants.h
@@ -0,0 +1,20 @@
+/*
+*	Project 2 constants
+*/
+
+/* Arduino pin assignments */
+#define LCD_RS 0
+#define LCD_EN 1
+#define LCD_DB0 2
+#define LCD_DB1 3
+#define LCD_DB2 4
+#define LCD_DB3 5
+
+#define COOKIE_SERVO 3
+
+#define DOOR_SERVO 4
+
+#define WIFI_MODULE1 12
+#define WIFI_MODULE2 13
+
+#define PIEZO_PIN 11
