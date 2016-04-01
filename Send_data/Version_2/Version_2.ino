b32edf8caf6beec33b3527600730af10020b9471
Anh Duc Bui <anhduc130@live.com>
2016-04-01 16:25:29 -0700

    Initial test version

---
+++ Send_data/Version_2/Version_2.ino
@@ -0,0 +1,87 @@
+#include <SoftwareSerial.h>
+ 
+#define DEBUG 1
+ 
+SoftwareSerial esp8266(1,0); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
+                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
+                             // and the RX line from the esp to the Arduino's pin 3
+                             
+#define SSID "TELUS6815"
+#define PASSWORD "bde840375e"
+#define BUFFER_SIZE 200
+char *response;
+
+void setup()
+{
+//  Serial.begin(115200);
+//  Serial.println(response);
+  esp8266.begin(115200); // your esp's baud rate might be different
+  esp8266.println(response);
+  sendData("AT+RST\r\n",1000,DEBUG); // reset module
+  sendData("AT+CWJAP=\"Connectify-me\",\"anhduc123\"\r\n",10000,DEBUG);
+  sendData("AT+CWMODE=3\r\n",2000,DEBUG); // configure as access point
+  sendData("AT+CIFSR\r\n",2000,DEBUG); // get ip address
+  sendData("AT+CIPMUX=1\r\n",2000,DEBUG); // configure for multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n",2000,DEBUG); // turn on server on port 80
+  sendData("AT+CIPSTART=0,\"TCP\",\"www.cookie4me.herokuapp.com\",80\r\n",10000,DEBUG);
+  sendData("AT+CIPSEND=0,64\r\n",1000,DEBUG);
+  sendData("GET /verify/123 HTTP/1.1\r\nHost:cookie4me.herokuapp.com\r\n\r\n",5000,DEBUG);
+  parseData("AT+CIPCLOSE\r\n",1000,DEBUG);
+}
+ 
+void loop()
+{
+
+}
+
+/*
+* Name: sendData
+* Description: Function used to send data to ESP8266.
+* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
+* Returns: The response from the esp8266 (if there is a reponse)
+*/
+void sendData(String command, const int timeout, boolean debug)
+{
+    char response[BUFFER_SIZE];
+    
+    esp8266.print(command); // send the read character to the esp8266
+    
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    { 
+    }
+}
+
+void parseData(String command, const int timeout, boolean debug)
+{
+    char response[BUFFER_SIZE];
+    int count = 0;
+    
+    esp8266.print(command); // send the read character to the esp8266
+    
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    {
+      while(esp8266.available())
+      {
+        
+        // The esp has data so display its output to the serial window 
+        for(int i = 0; i < BUFFER_SIZE; i++){
+          char c = (char) esp8266.read(); // read the next character.
+          response[i] = c;
+        }
+      }  
+    }
+//    esp8266.println("Start of the response: ");
+//    while (count < BUFFER_SIZE){
+//      esp8266.println(response[count]);
+//      count++;
+//    }
+    
+    if(debug)
+    {
+      Serial.print(response);
+    }
+}
