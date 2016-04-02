ceed697e0095b402ae4e4d1ff6b298d9ceecacb4
Andy Lin <andylin1996@gmail.com>
2016-04-02 02:24:47 -0700

    Fixed cookie count issue. This is a working version for cookie count display

--- Send_data/Version_2/Version_2.ino
+++ Send_data/Version_2/Version_2.ino
@@ -9,24 +9,24 @@ SoftwareSerial esp8266(1,0); // make RX Arduino line is pin 2, make TX Arduino l
 #define SSID "TELUS6815"
 #define PASSWORD "bde840375e"
 #define BUFFER_SIZE 201
-char *response;
+char cookies[2];
 
 void setup()
 {
-//  Serial.begin(115200);
-//  Serial.println(response);
   esp8266.begin(115200); // your esp's baud rate might be different
-  esp8266.println(response);
+
   sendData("AT+RST\r\n",1000,DEBUG); // reset module
   sendData("AT+CWJAP=\"Connectify-me\",\"anhduc123\"\r\n",10000,DEBUG);
   sendData("AT+CWMODE=3\r\n",2000,DEBUG); // configure as access point
   sendData("AT+CIFSR\r\n",2000,DEBUG); // get ip address
   sendData("AT+CIPMUX=1\r\n",2000,DEBUG); // configure for multiple connections
   sendData("AT+CIPSERVER=1,80\r\n",2000,DEBUG); // turn on server on port 80
-  //sendData("AT+CIPSTART=0,\"TCP\",\"www.cookie4me.herokuapp.com\",80\r\n",3000,DEBUG);//10000
-  //sendData("AT+CIPSEND=0,61\r\n",1000,DEBUG);
-  //sendData("GET /add/abc HTTP/1.1\r\nHost:cookie4me.herokuapp.com\r\n\r\n",3000,DEBUG);
-  add("abc");
+
+  
+//  sendData("AT+CIPSTART=0,\"TCP\",\"www.cookie4me.herokuapp.com\",80\r\n",3000,DEBUG);//10000
+//  sendData("AT+CIPSEND=0,61\r\n",1000,DEBUG);
+//  sendData("GET /add/abc HTTP/1.1\r\nHost:cookie4me.herokuapp.com\r\n\r\n",3000,DEBUG);
+//  add("abc");
 //  sendData("AT+CIPCLOSE=0\r\n",2000,DEBUG);
 //  sendData("AT+CIPCLOSE=0\r\n",2000,DEBUG);
   
@@ -35,12 +35,16 @@ void setup()
 //  sendData("GET /verify/abc HTTP/1.1\r\nHost:cookie4me.herokuapp.com\r\n\r\n",5000,DEBUG);//5000
 //  parseData("AT+CIPCLOSE=1\r\n",2000,DEBUG);
 //  sendData("AT+CIPCLOSE=1\r\n",2000,DEBUG);
-  verify("abc");
+//  verify("abc");
+
+  add("123");
+  verify("123");
 }
  
 void loop()
 {
-
+//  add("abc");
+//  verify("abc");
 }
 
 /*
@@ -65,9 +69,13 @@ void sendData(String command, const int timeout, boolean debug)
 void parseData(String command, const int timeout, boolean debug)
 {
     char response[BUFFER_SIZE];
+    char cookieCount[10];
     int count = 0;
+    int test = 0;
     
     esp8266.print(command); // send the read character to the esp8266
+    
+    //esp8266.end();
     
     long int time = millis();
     
@@ -75,21 +83,72 @@ void parseData(String command, const int timeout, boolean debug)
     {
       while(esp8266.available())
       {
-        
         // The esp has data so display its output to the serial window 
         for(int i = 0; i < BUFFER_SIZE; i++){
           char c = (char) esp8266.read(); // read the next character.
           response[i] = c;
         }
-      }  
+       }  
     }
-    esp8266.println("Start of the response: ");
-    esp8266.println(response[0]);
+
+    esp8266.end();
+    Serial.begin(115200);
+    while (response[count] != ','){
+      cookieCount[count] = response[count];
+      count++;
+    }
+    Serial.print("\nThe cookie count is: ");
+
+    for (int i = 0; i < count; i++){
+      Serial.write(cookieCount[i]);
+    }
+
+
+//    while (test < BUFFER_SIZE){
+//      Serial.print("The character at index ");
+//      Serial.print(test);
+//      Serial.print(" is ");
+//      Serial.println(response[test]);
+//      test++;
+//    }
+
     
-    if(debug)
-    {
-      Serial.print(response);
-    }
+
+
+    // Start here
+//    while(response[0] == '3' || response[0] == '4' || response[0] == '5'){
+//    if (response[1] != ','){
+//      cookies[0] = response[0];
+//      cookies[1] = response[1];
+//      break;
+//    }
+//    else{
+//      cookies[0] == response[0];
+//      break;
+//    }
+//    }
+    
+
+//    esp8266.println(cookies[0]);
+//    esp8266.println(cookies[1]);
+    
+   // My thing goes here
+//    esp8266.println(response[0]);
+//    esp8266.println(response[1]);
+//
+//    char cookies[2];
+//    cookies[0] = response[0];
+//    esp8266.println(cookies);
+//    int i = 0;
+//    while(true){
+//      if(response[i+1] != ','){
+//        cookies[i+1] = response[i+1];
+//        i++; 
+//      }
+//      else
+//        break;
+//    }
+//    esp8266.println(cookies);
 }
 
 void add(String key_tag){
@@ -115,3 +174,11 @@ void verify(String key_tag){
   parseData("AT+CIPCLOSE=1\r\n",2000,DEBUG);
   sendData("AT+CIPCLOSE=1\r\n",2000,DEBUG);
 }
+
+void findCookies(void){
+  
+}
+
+
+
+
